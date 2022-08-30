da26a588825ea0b3380ca86eec2da572921db839
Linnea Winqvist <winqvist.linnea@elev.ga.ntig.se>
2022-08-30 13:04:44 +0200

    yydyfydy

--- bash-a-monster/Program.cs
+++ bash-a-monster/Program.cs
@@ -1,3 +1,12 @@
 ﻿Player thePlayer  = new Player();
 Enemy theEnemy = new Enemy();
 
+Console.WriteLine($"{thePlayer.name} has {thePlayer.health} health points");
+Console.WriteLine($"{theEnemy.name} has {theEnemy.health} health points");
+
+Console.WriteLine("Attack!!!");
+theEnemy.health -= 10;
+
+Console.WriteLine($"{theEnemy.name} now has {theEnemy.health} health points");
+
+Console.ReadLine();
